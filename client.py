tested gradient inversion - bug fix

client.py
@@ -4,16 +4,17 @@ from torch.utils.data import DataLoader, Dataset
 
 import numpy as np
 import pandas as pd
+import random
 
 import GTKutils
 
 
-# torch.manual_seed(0)
-#
-# g = torch.Generator()
-# g.manual_seed(0)
-#
-# np.random.seed(0)
+torch.manual_seed(0)
+
+g = torch.Generator()
+g.manual_seed(0)
+
+np.random.seed(0)
 
 
 class DatasetSplit(Dataset):
@@ -69,15 +70,23 @@ class GKTClientTrainer(object):
     def update_large_model_logits(self, logits):
         self.server_logits_dict = logits
 
+    # for REPRODUCIBILITY https://pytorch.org/docs/stable/notes/randomness.html
+    def seed_worker(worker_id):
+        worker_seed = torch.initial_seed() % 2**32
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
+
     def train_test(self, train_dataset, test_dataset, idxs):
         """
         Returns train and test dataloaders for a given dataset
         and user indexes.
         """
         trainloader = DataLoader(DatasetSplit(train_dataset, idxs),
-                                 batch_size=self.local_batch_size, shuffle=True)
+                                 batch_size=self.local_batch_size, shuffle=True, generator=g,
+                                 worker_init_fn=self.seed_worker)
         testloader = DataLoader(test_dataset,
-                                batch_size=self.local_batch_size, shuffle=False)
+                                batch_size=self.local_batch_size, shuffle=False, generator=g,
+                                worker_init_fn=self.seed_worker)
 
         return trainloader, testloader
 

models.py
@@ -9,7 +9,7 @@ norm_type = "BatchNorm"
 
 
 # function that let you choose between BatchNorm and GroupNorm
-def Norm(planes, type="BatchNorm", num_groups=8):
+def Norm(planes, type="BatchNorm", num_groups=4):
     if type == "BatchNorm":
         return nn.BatchNorm2d(planes)
     if type == "GroupNorm":

update.py
@@ -5,6 +5,7 @@ from torch import nn
 from torch.utils.data import DataLoader, Dataset, SubsetRandomSampler
 
 import numpy as np
+import random
 
 torch.manual_seed(0)
 
@@ -52,6 +53,13 @@ class LocalUpdate(object):
         self.trainloader, self.testloader = self.train_test(
             dataset, list(idxs))  # get train, valid sets
 
+    # for REPRODUCIBILITY https://pytorch.org/docs/stable/notes/randomness.html
+    def seed_worker(worker_id):
+        worker_seed = torch.initial_seed() % 2**32
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
+
+
     def train_test(self, dataset, idxs):
         """
         Returns train, validation and test dataloaders for a given dataset
@@ -61,9 +69,11 @@ class LocalUpdate(object):
         idxs_test = idxs[int(0.9 * len(idxs)):]
 
         trainloader = DataLoader(DatasetSplit(dataset, idxs_train),
-                                 batch_size=self.local_batch_size, shuffle=True, generator=g)
+                                 batch_size=self.local_batch_size, shuffle=True, generator=g,
+                                 worker_init_fn=self.seed_worker)
         testloader = DataLoader(DatasetSplit(dataset, idxs_test),
-                                batch_size=max(int(len(idxs_test) / 10), 1), shuffle=False, generator=g)
+                                batch_size=max(int(len(idxs_test) / 10), 1), shuffle=False, generator=g,
+                                worker_init_fn=self.seed_worker)
 
         return trainloader, testloader
 